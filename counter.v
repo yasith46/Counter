7253e7484184434db419354c411db3c6f7d9c9ae
Yasith Silva <133279208+yasith46@users.noreply.github.com>
2024-08-30 20:39:05 +0530

    reformatted counter.v

--- counter.v
+++ counter.v
@@ -8,27 +8,23 @@ module counter(
 		.BAUD(BAUD)
 	);
 	
-	always@(posedge BAUD)
-		begin
-			if (SEL==1'b0)	// Up counter
-				begin
-					if (OUT[18] == 1'b1)
-						OUT <= 19'b0;
-					else
-						OUT <= OUT + 1'b1;
-						
-					if (RST == 1'b0)
-						OUT <= 19'b0;
-				end
+	always@(posedge BAUD) begin
+		if (SEL==1'b0)	begin
+			if (OUT[18] == 1'b1)
+				OUT <= 19'b0;
 			else
-				begin
-					if (OUT == 18'b0)
-						OUT[18] <= 1'b1;
-					else
-						OUT <= OUT - 1'b1;
-						
-					if (RST == 1'b0)
-						OUT[17:0] <= 18'b111111111111111111;
-				end
+				OUT <= OUT + 1'b1;
+				
+			if (RST == 1'b0)
+				OUT <= 19'b0;
+		end else begin
+			if (OUT == 18'b0)
+				OUT[18] <= 1'b1;
+			else
+				OUT <= OUT - 1'b1;
+				
+			if (RST == 1'b0)
+				OUT[17:0] <= 18'b111111111111111111;
 		end
+	end
 endmodule 
